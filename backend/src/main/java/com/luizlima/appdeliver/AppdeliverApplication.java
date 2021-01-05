46770af8748f8a26c2f253db352fca22401cad17
Luiz Lima <luizlima.udi@gmail.com>
2021-01-05 15:11:33 -0300

    Project created

---
+++ backend/src/main/java/com/luizlima/appdeliver/AppdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.luizlima.appdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luizlima/appdeliver/AppdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizlima.appdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
