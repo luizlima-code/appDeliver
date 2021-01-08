25c5a1c92f55f0681e655ed8d63acd6ef8c54129
Luiz Lima <luizlima.udi@gmail.com>
2021-01-08 15:05:06 -0300

    Primeira parte FrontEnd Finish

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,23 @@
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
 import './styles.css';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts()
+    .then(response => setProducts(response.data))
+    .catch(error => console.log(error))
+  }, []);
+  
   return(
     <div className="orders-container">
       <StepsHeader />
+      <ProductsList products={products} />
     </div>
   )
 }
