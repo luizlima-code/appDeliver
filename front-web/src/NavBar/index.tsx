7b1daf483d909509d52f9eb6b84fb01cb5548693
Luiz Lima <luizlima.udi@gmail.com>
2021-01-08 14:00:14 -0300

    Add routes and StepsHeader

--- front-web/src/NavBar/index.tsx
+++ front-web/src/NavBar/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css';
 import { ReactComponent as Logo } from './logo.svg'
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return(
     <nav className="main-navbar">
       <Logo />
-      <a href="home" className="logo-text">APP Deliver</a>
+      <Link to="/" className="logo-text">APP Deliver</Link>
     </nav>
   )
 }
